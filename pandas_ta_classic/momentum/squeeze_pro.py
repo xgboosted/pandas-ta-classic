777c86b01de006262549cd80574ee2b7285bfed8
Golam Rashed <golam.rashed@outlook.com>
2025-08-02 01:07:04 +1000

    Updates import to use correct NumPy nan alias
    Replaces deprecated or incorrect import of NaN with the standard
    NumPy nan alias to ensure compatibility and consistency.

--- pandas_ta_classic/momentum/squeeze_pro.py
+++ pandas_ta_classic/momentum/squeeze_pro.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from numpy import NaN as npNaN
+from numpy import nan as npNaN
 from pandas import DataFrame
 from pandas_ta_classic.momentum import mom
 from pandas_ta_classic.overlap import ema, sma
